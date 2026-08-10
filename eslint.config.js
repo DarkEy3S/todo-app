01dbebf58c37518be86d3db89a8bcebbad4edc23
DarkEy3S <Sevak.martirosyan1@gmail.com>
2026-08-10 14:15:37 +1000

    add: In eslint.config.js added endOfLine: auto

--- eslint.config.js
+++ eslint.config.js
@@ -1,50 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import pluginReact from 'eslint-plugin-react'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
-    globalIgnores(['dist']),
-    {
-        files: ['**/*.{ts,tsx}'],
-        extends: [
-            js.configs.recommended,
-            tseslint.configs.recommended,
-            reactHooks.configs['recommended-latest'],
-            reactRefresh.configs.vite,
-        ],
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
 
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
-        },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-    eslintPluginPrettierRecommended,
-    tseslint.configs.recommended,
-    {
-        ...pluginReact.configs.flat.recommended,
-        rules: {
-            // отключаем старое правило для React 17+
-            'react/react-in-jsx-scope': 'off',
-            'no-console': 'error',
-            'prettier/prettier': [
-                'error',
-                {
-                    singleQuote: false,
-                    printWidth: 130,
-                    tabWidth: 2,
-                    semi: true,
-                },
-            ],
+  },
+  eslintPluginPrettierRecommended,
+  tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      // отключаем старое правило для React 17+
+      "react/react-in-jsx-scope": "off",
+      "no-console": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: false,
+          printWidth: 130,
+          tabWidth: 2,
+          semi: true,
+          endOfLine: "auto",
         },
-        settings: {
-            react: {
-                version: 'detect',
-            },
-        },
+      ],
     },
-])
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
