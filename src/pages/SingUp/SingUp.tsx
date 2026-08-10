6f2e353f7d14d3b0d9f30761fe25e2914c5cba6b
DarkEy3S <Sevak.martirosyan1@gmail.com>
2026-08-10 17:20:39 +1000

    feat: enhance sign-up form with password visibility toggle and validation messages
    - Added password visibility toggle using eye icons.
    - Implemented email and password validation with error messages.
    - Updated CSS for input error states and added transition effects.
    - Refactored state management for email and password inputs.

--- src/pages/SingUp/SingUp.tsx
+++ src/pages/SingUp/SingUp.tsx
@@ -3,16 +3,26 @@ import { Link } from "react-router-dom";
 import { useId } from "react";
 import btn from "../../assets/buttons.module.css";
 import { useState } from "react";
+import { PasswordIconEyeClose, PasswordIconEyeOpen } from "../../components/icons";
 
 export const SingUp = () => {
   const singUpEmailId: string = useId();
   const singUpPasswordId: string = useId();
   const singUpPrivacyId: string = useId();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [emailValidity, setEmailValidity] = useState(false);
+  const [passwordValue, setPasswordValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
   const [privacyCheckbox, setPrivacyCheckbox] = useState(false);
+  const [emailTouched, setEmailTouched] = useState(false);
+  const [passworTouched, setPassworTouched] = useState(false);
+  const [passwordVisible, isPasswordVisible] = useState(false);
 
-  const isSubmitDisabled = email.trim() === "" || password.trim() === "" || privacyCheckbox === false;
+  const isEmailValid = emailValidity === false;
+  const isPasswordValid = passwordValue.trim().length < 4;
+
+  const isSubmitDisabled = isEmailValid || isPasswordValid || privacyCheckbox === false;
+  const isEmailError = isEmailValid && emailTouched;
+  const isPasswordError = isPasswordValid && passworTouched;
 
   return (
     <section className={cls.singUp}>
@@ -21,30 +31,62 @@ export const SingUp = () => {
         <form className={cls.singUpForm} action="">
           <label htmlFor={singUpEmailId}>
             <span>Email</span>
-            <input
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              id={singUpEmailId}
-              placeholder={"Email"}
-              aria-placeholder={"Email"}
-              type="email"
-            />
+            <div className={cls.wrapper}>
+              <input
+                className={`${isEmailError && cls.singUpFormInputError}`}
+                value={emailValue}
+                onChange={(e) => {
+                  const cleaned = e.target.value.replaceAll(" ", "");
+                  e.target.value = cleaned;
+                  setEmailValue(cleaned);
+                  setEmailValidity(e.target.checkValidity());
+                }}
+                formNoValidate={emailValidity}
+                id={singUpEmailId}
+                placeholder={"Email"}
+                aria-placeholder={"Email"}
+                type="email"
+                onBlur={() => {
+                  setEmailTouched(true);
+                }}
+                required
+              />
+            </div>
+
+            {isEmailError && <span className={cls.singUpFormInputMessage}>Invalid email format.</span>}
           </label>
           <label htmlFor={singUpPasswordId}>
             <span>Password</span>
-            <input
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              minLength={4}
-              placeholder={"Password"}
-              aria-placeholder={"Password"}
-              type="password"
-              id={singUpPasswordId}
-            />
+            <div className={cls.wrapper}>
+              <input
+                className={`${isPasswordError && cls.singUpFormInputError}`}
+                value={passwordValue}
+                onChange={(e) => {
+                  const cleaned = e.target.value.replaceAll(" ", "");
+                  e.target.value = cleaned;
+                  setPasswordValue(cleaned);
+                }}
+                minLength={4}
+                placeholder={"Password"}
+                aria-placeholder={"Password"}
+                type={passwordVisible ? "text" : "password"}
+                id={singUpPasswordId}
+                onBlur={() => {
+                  setPassworTouched(true);
+                }}
+              />
+              <button
+                className={cls.singUpFormEye}
+                type="button"
+                onClick={() => {
+                  isPasswordVisible((v) => !v);
+                }}
+              >
+                {passwordVisible ? <PasswordIconEyeClose /> : <PasswordIconEyeOpen />}
+              </button>
+            </div>
+
+            {isPasswordError && <span className={cls.singUpFormInputMessage}>4 characters minimum.</span>}
           </label>
           <label className={cls.singUpPrivacy} htmlFor={singUpPrivacyId}>
             <input
