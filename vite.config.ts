919205f81c973acbc8de1cd7475813bc5adb327d
DarkEy3S <Sevak.martirosyan1@gmail.com>
2026-08-10 13:52:32 +1000

    fix: fix docker and vite prots

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: Number(process.env.PORT) || 3000,
     watch: {
       usePolling: true,
     },
-    host: true,
   },
 });
