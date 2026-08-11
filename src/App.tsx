dfcaf45ccf716d424f600895611ee1c8cb46fdaf
Sevak Home PC <Sevak.martirosyan1@gmail.com>
2026-08-11 23:05:12 +1000

    feat: add todos page layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 import { SignUp } from "./pages/SignUp";
 import { SignIn } from "./pages/SignIn";
 import { Privacy } from "./pages/Privacy";
+import { Todos } from "./pages/Todos";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/privacy" element={<Privacy />} />
+          <Route path="/todos" element={<Todos />} />
           <Route path="*" element={<div>404!</div>} />
         </Route>
       </Routes>
---
+++ src/pages/Todos/Todos.tsx
@@ -0,0 +1,68 @@
+import cls from "./Todos.module.css";
+import { IconNoProjects, IconSearch, IconSort } from "../../components/icons";
+import btn from "../../assets/buttons.module.css";
+
+export const Todos = () => {
+  return (
+    <section className={cls.todos}>
+      <div className="container">
+        <div className={cls.todosContent}>
+          <div className={cls.todosTop}>
+            <div className={cls.todosSearch}>
+              <IconSearch className={cls.todoSearchIcon} />
+              <input className={cls.todosSearchInput} type={"search"} placeholder="Search some todo..." />
+            </div>
+          </div>
+          <div className={cls.todosBottom}>
+            <div className={cls.todosTitleWrapper}>
+              <h1 className={cls.todosTitle}>Todos</h1>
+              <div className={`${cls.todosListSort} ${cls.todosListSortDesktopHidden}`}>
+                <button className={cls.todosListSortArrow} type={"button"}>
+                  <IconSort />
+                </button>
+                <button className={cls.todosListSortText} type={"button"}>
+                  Sort by
+                </button>
+              </div>
+            </div>
+            <div className={cls.todosAdd}>
+              <input className={cls.todosAddInput} type="text" placeholder={"Add a todo"} />
+              <button disabled={true} className={`${cls.todosAddButton} ${btn.btn}`} type={"button"}>
+                Add
+              </button>
+            </div>
+            <div className={cls.todosList}>
+              <div className={cls.todosListTop}>
+                <div className={cls.todosListStatus}>
+                  <button className={`${cls.todosListStatusButton} ${cls.todosListStatusButtonActive}`} type={"button"}>
+                    All
+                  </button>
+                  <button className={cls.todosListStatusButton} type={"button"}>
+                    Active
+                  </button>
+                  <button className={cls.todosListStatusButton} type={"button"}>
+                    Completed
+                  </button>
+                </div>
+                <div className={`${cls.todosListSort} ${cls.todosListSortMobileHidden}`}>
+                  <button className={cls.todosListSortArrow} type={"button"}>
+                    <IconSort />
+                  </button>
+                  <button className={cls.todosListSortText} type={"button"}>
+                    Sort by
+                  </button>
+                </div>
+              </div>
+              <div className={cls.todosListBottom}>
+                <div className={cls.todosNoProjects}>
+                  <IconNoProjects />
+                  <span>No todos yet</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
